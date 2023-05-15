423351d4a5506dc743d879d2c0ef95ec0bf09792
vipin15053 <vipinkumar.yadav@accionlabs.com>
2023-05-15 14:07:14 +0530

    changes are commitnign

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoal, setGoal] = useState("");
+  const [listGoal, pushGoal] = useState([]);
+  const deleteGoal = (goal) => {
+    pushGoal(listGoal.filter((itm) => itm !== goal));
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.goalContainer}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Enter your goal."
+          value={enteredGoal}
+          onChangeText={(inptText) => setGoal(inptText)}
+        />
+        <Button
+          title="Add Goal"
+          onPress={() => {
+            pushGoal((prevState) =>
+              enteredGoal && !listGoal.includes(enteredGoal)
+                ? [...prevState, enteredGoal]
+                : [...prevState]
+            );
+            setGoal("");
+          }}
+        />
+      </View>
+      <View style={styles.listGoalContainer}>
+        <Text>List of your goal.....</Text>
+        <ScrollView>
+          {listGoal.length > 0 &&
+            listGoal.map((item) => {
+              return (
+                <Pressable onPress={() => deleteGoal(item)} key={item}>
+                  <View style={styles.goalItem}>
+                    <Text style={styles.goalText}>{item}</Text>
+                  </View>
+                </Pressable>
+              );
+            })}
+        </ScrollView>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginHorizontal: 20,
+    marginTop: 50,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  goalContainer: {
+    flex: 2,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+  },
+  inputText: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "75%",
+    padding: 3,
+    marginRight: 5,
+  },
+  listGoalContainer: {
+    flex: 11,
+    flexDirection: "column",
+  },
+  goalItem: {
+    padding: 8,
+    marginVertical: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    color: "white",
   },
 });
